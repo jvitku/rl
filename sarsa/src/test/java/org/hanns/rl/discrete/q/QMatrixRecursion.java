b5796b768c4f31c21fe41fa42431f3dddab4467a
Jaroslav Vitku <jarda.vitku@gmail.com>
2013-12-31 05:03:06 +0100

    test

--- sarsa/src/test/java/org/hanns/rl/discrete/q/QMatrixRecursion.java
+++ sarsa/src/test/java/org/hanns/rl/discrete/q/QMatrixRecursion.java
@@ -1,27 +1,33 @@
 package org.hanns.rl.discrete.q;
 
+import static org.junit.Assert.*;
+
 import org.hanns.rl.discrete.learningAlgorithm.qlearning.model.impl.Dimension;
 import org.junit.Test;
 
 public class QMatrixRecursion {
-	
+
 	@Test
 	public void init(){
-		
+
 		int []dims = new int[]{2,3,4};
-		
-		Dimension dd = new Dimension(dims,0,-2);
-		
+
+		Dimension<Double> dd = new Dimension<Double>(dims,0,new Double(-2));
+
 		System.out.println("XXXXXXXXXXXXXX");
-		
-		
+
+
 		int[] coords = new int[]{0,2,3};
-		dd.setValue(coords, 7);
-	
+		dd.setValue(coords, new Double(7));
+
 		System.out.println("XXXXXXXXXXXXXX");
-		
-		System.out.println(dd.readValue(coords));
-		
+
+		assertTrue(dd.readValue(coords)==7);
+		System.out.println("reading value: "+dd.readValue(coords));
+
+
+		assertTrue(dd.readValue(new int[]{1,1,1})==-2);
+
 	}
 
 }

1f3a5639910272c499d606c34c15df935c5b111d
Jaroslav Vitku <jarda.vitku@gmail.com>
2014-01-05 14:53:07 +0100

    parameters work, adding range check where needed

--- sarsa/src/main/java/org/hanns/rl/discrete/actionSelectionStrategy/epsilonGreedy/config/impl/BasicConfig.java
+++ sarsa/src/main/java/org/hanns/rl/discrete/actionSelectionStrategy/epsilonGreedy/config/impl/BasicConfig.java
@@ -10,27 +10,56 @@ import org.hanns.rl.discrete.actionSelectionStrategy.epsilonGreedy.config.Epsilo
  */
 public class BasicConfig implements EpsilonGreedyConfig {
 
-	private double epsilon = 0;
-	private double minEpsilon = 0;
+	public static final double DEF_EPSILON = 0.5;
+	public static final double DEF_MINEPSILON = 0.2;
+
+
+	private double epsilon = DEF_EPSILON;
+	private double minEpsilon = DEF_MINEPSILON;
 	private boolean explorationEnabled = true;
 
 	@Override
-	public void setMinEpsilon(double min) { this.minEpsilon = min; }
+	public void setMinEpsilon(double min) {
+		if(!this.checkRange("minEpsilon", 0, 1, min))
+			return;
+		this.minEpsilon = min;
+	}
 
 	@Override
 	public double getMinEpsilon(double min) { return this.minEpsilon; }
 
 	@Override
-	public void setExplorationEnabled(boolean enable) { this.explorationEnabled = enable;	}
+	public void setExplorationEnabled(boolean enable) { this.explorationEnabled = enable; }
 
 	@Override
 	public boolean getExplorationEnabled() { return this.explorationEnabled; }
 
 	@Override
-	public void setEpsilon(double value) { this.epsilon = value;	}
+	public void setEpsilon(double value) {
+		if(!this.checkRange("epsilon", 0, 1, value))
+			return;
+		this.epsilon = value;
+	}
 
 	@Override
 	public double getEpsilon() { return this.epsilon; }
+
+
+	/**
+	 * Check if a new value to a given parameter is in correct range, if not, log error
+	 * @param paramName name of the parameter
+	 * @param from lower bound inclusive
+	 * @param to upper bound inclusive
+	 * @param newVal new value to be set
+	 * @return true if the new value is in range
+	 */
+	public boolean checkRange(String paramName, double from, double to, double newVal){
+		if(newVal>=from && newVal<=to)
+			return true;
+		System.err.println("Config ERROR, the parameter "+paramName+" should be from" +
+				" interval ["+from+","+to+"], not "+newVal);
+		return false;
+	}
 
 }
 
--- sarsa/src/main/java/org/hanns/rl/discrete/learningAlgorithm/qlearning/config/impl/BasicConfiguration.java
+++ sarsa/src/main/java/org/hanns/rl/discrete/learningAlgorithm/qlearning/config/impl/BasicConfiguration.java
@@ -12,12 +12,17 @@ public class BasicConfiguration implements QLearningConfig{
 
 	private boolean learningEnabled = true;
 
+	public static final double DEF_ALPHA=0.5;
+	public static final double DEF_GAMMA=0.3;
+	
 	// stochastic return predictor settings
-	private double alpha;
-	private double gamma;
+	private double alpha = DEF_ALPHA;
+	private double gamma = DEF_GAMMA;
 
 	@Override
 	public void setAlpha(double alpha) {
+		if(!this.checkRange("alpha", 0, 1, alpha))
+			return;
 		this.alpha = alpha;
 		this.fireParameterChanged();
 	}
@@ -27,6 +32,8 @@ public class BasicConfiguration implements QLearningConfig{
 
 	@Override
 	public void setGamma(double gamma) {
+		if(!this.checkRange("gamma", 0, 1, gamma))
+			return;
 		this.gamma = gamma;
 		this.fireParameterChanged();
 	}
@@ -45,5 +52,21 @@ public class BasicConfiguration implements QLearningConfig{
 
 	@Override
 	public void fireParameterChanged() {}
+
+	/**
+	 * Check if a new value to a given parameter is in correct range, if not, log error
+	 * @param paramName name of the parameter
+	 * @param from lower bound inclusive
+	 * @param to upper bound inclusive
+	 * @param newVal new value to be set
+	 * @return true if the new value is in range
+	 */
+	public boolean checkRange(String paramName, double from, double to, double newVal){
+		if(newVal>=from && newVal<=to)
+			return true;
+		System.err.println("Config ERROR, the parameter "+paramName+" should be from" +
+				" interval ["+from+","+to+"], not "+newVal);
+		return false;
+	}
 
 }

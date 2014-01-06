da9f478af3cc788e88df30eba9df4fd0973c8965
Jaroslav Vitku <jarda.vitku@gmail.com>
2014-01-06 03:38:55 +0100

    RL node successfully connected to function generators in the Nengoros, TODO: some results

---
+++ sarsa/python/nr-demo/sarsa.py
@@ -0,0 +1,53 @@
+# Create the NeuralModule which implements discrete RL algorithm - Q(lambda) - Q-learning with eligibility traces
+#
+# starts: 
+#   -ROS-java node which receives description of wold state (together with a reinforcement) and publishes (executes) one of possible actions
+#   -NeuralModule with modem that communicates with the ROS node (represents ROS node in the Nengoros)
+#
+# by Jaroslav Vitku [vitkujar@fel.cvut.cz]
+
+import nef
+from ca.nengo.math.impl import FourierFunction
+from ca.nengo.model.impl import FunctionInput
+from ca.nengo.model import Units
+from ctu.nengoros.modules.impl import DefaultNeuralModule as NeuralModule
+from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
+from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
+
+from org.hanns.rl.discrete.ros.sarsa import QLambda
+
+import rl_sarsa
+
+
+net=nef.Network('Demo of NeuralModule which implements discrete RL')
+net.add_to_nengo()  
+
+################# setup the ROS utils (optional) 
+#RosUtils.setAutorun(False)     # Do we want to autorun roscore and rxgraph? (tru by default)
+#RosUtils.prefferJroscore(True)  # preffer jroscore before the roscore? 
+
+finderA = rl_sarsa.qlambda("RL",2, 4)   # 2 state variables, 4 actions
+many=net.add(finderA)
+
+#Create a white noise input function with params: baseFreq, maxFreq [rad/s], RMS, seed
+generator=FunctionInput('StateGenerator', [FourierFunction(.1, 10,1, 12),
+    FourierFunction(.5, 11,1.6, 17),FourierFunction(.5, 11,1.6, 17)],Units.UNK) 
+    
+reward=FunctionInput('RewardGenerator', [FourierFunction(.1, 10,1, 12),
+        FourierFunction(.5, 11,1.6, 17),FourierFunction(.5, 11,1.6, 17)],Units.UNK)
+        
+net.add(generator)
+net.add(reward)
+net.connect(generator,	finderA.newTerminationFor(QLambda.topicDataIn,[0,1,1]))
+net.connect(reward,	finderA.newTerminationFor(QLambda.topicDataIn,[1,0,0]))
+#net.connect(generator,	finderB.getTermination('org/hanns/demonodes/pubsub/IN')) 		# connect generator to the termnation on our neural module
+
+# create ANN with 100 neurons which approximates it's input 
+A=net.make('A',neurons=200,dimensions=2,radius=8)    				# identity transformation implemented by population of neurons
+#net.connect(finderA.getOrigin('org/hanns/demonodes/pubsub/OUT'),A)	# connect the origin on our module to the network
+
+#B=net.make('B',neurons=200,dimensions=2,radius=8)    				
+#net.connect(finderB.getOrigin('org/hanns/demonodes/pubsub/OUT'),	B)						
+
+
+print 'Configuration complete.'
--- sarsa/python/rosnodes/rl_sarsa.py
+++ sarsa/python/rosnodes/rl_sarsa.py
@@ -1,79 +1,30 @@
-# This is python interface for the project demonodes/basic. 
-# This helps using particular ROsJava nodes in Jython scripts for Nengo simulator.
+# Jython helper for launching a NeuralModule which implements discrete RL algorithm - Q(lambda) - Q-learning with eligibility traces
 #
-# Import this in your scirpt in order to use it, for examples see ../nr-demo/ scripts.
+# starts: 
+#   -ROS-java node which receives description of wold state (together with a reinforcement) and publishes (executes) one of possible actions
+#   -NeuralModule with modem that communicates with the ROS node (represents ROS node in the Nengoros)
 #
-#
-# author Jaroslav Vitku
+# by Jaroslav Vitku [vitkujar@fel.cvut.cz]
 
 import nef
 from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
 from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
-# Note that modules without outputs (Nengo-decoder) cannot use synchronous mode (DefaultNeuralModule).
-# The simulator waits each simulation step for the output in the synchronous mode, this case:
 from ctu.nengoros.modules.impl import DefaultNeuralModule as NeuralModule
 
+from org.hanns.rl.discrete.ros.sarsa import QLambda
+
 # java classes
-minmaxint =   	"org.hanns.demonodes.pubsub.MinMaxInt"
-mmf = 			"org.hanns.demonodes.pubsub.MinMaxFloat"
+node = "org.hanns.rl.discrete.ros.sarsa.QLambda"
 
-time = 			"org.hanns.demonodes.time.TimeAwareNode"
-
-
-# Finds min and max values form vector of 4 input floats and publishes them as Integers
-# Asynchronous communication with Nengo.
-def async_minmaxint_node(name):
-	g = NodeGroup("AsynMinMaxInt", True);        			# create independent (True) group called..
-	g.addNode(minmaxint, "AsynMinMaxInt", "java");     		# start java node and name it finder
-	module = NeuralModule(name+'_AsynMinMaxInt', g, False)   # construct the Neural Module
-	# add encoder to the module (input)
-	# It is output on modem which is connected to input (topic name) to the ROS node
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)  	# ..called TERMINATION of SimpleNode
-	# Add decoder to the module (output):
-	# It is input if modem which is connected to output (topic name) of the ROS node
-	module.createDecoder("org/hanns/demonodes/pubsub/OUT", "int", 2)    # ..called ORIGIN of SimpleNode
-	return module
-
-
-# The same as above with synchronous communication
-def sync_minmaxint_node(name):
-	g = NodeGroup("SynMinMaxInt", True);        			
-	g.addNode(minmaxint, "SynMinMaxInt", "java");     		
-	module = NeuralModule(name+'_SynMinMaxInt', g)    	# here: Nengo waits each step for response from node
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)  			
-	module.createDecoder("org/hanns/demonodes/pubsub/OUT", "int", 2)    			
-	return module
-
-def sync_minmaxfloat_node(name):
-	g = NodeGroup("SynMinMaxFloat", True);        			
-	g.addNode(mmf, "SynMinMaxFloat", "java");     		
-	module = NeuralModule(name+'_SynMinMaxFloat', g)    	# here: Nengo waits each step for response from node
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)  			
-	neuron.createDecoder("org/hanns/demonodes/pubsub/OUT", "float", 2)  # add FLOAT publisher
-	return module
-
-def async_minmaxfloat_node(name):
-	g = NodeGroup("AsynMinMaxFloat", True);        			
-	g.addNode(mmf, "AsynMinMaxFloat", "java");     		
-	module = NeuralModule(name+'_AsynMinMaxFloat', g, False)    	
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)  			
-	module.createDecoder("org/hanns/demonodes/pubsub/OUT", "float", 2)    		
-	return module
-
-def async_timeaware_node(name):
-	g = NodeGroup("AsynTimeAwareNode", True);        			
-	g.addNode(time, "AsynTimeAwareNode", "java");     		
-	module = NeuralModule(name+'_AsynTimeAwareNode', g, False)    	
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)			
-	module.createDecoder("org/hanns/demonodes/pubsub/OUT", "float", 2)
-	return module
+# Synchronous NeuralModule implementing QLambda algorithm
+def qlambda(name, noStateVars=2, noActions=4):
+	g = NodeGroup("QLambda", True);
+	g.addNode(node, "QLambda", "java");
+	module = NeuralModule(name+'_QLambda', g, False)
 	
-def sync_timeaware_node(name):
-	g = NodeGroup("SynMinMaxFloat", True);        			
-	g.addNode(time, "SynMinMaxFloat", "java");     		
-	module = NeuralModule(name+'_SynMinMaxFloat', g)    	
-	module.createEncoder("org/hanns/demonodes/pubsub/IN", "float", 4)  			
-	module.createDecoder("org/hanns/demonodes/pubsub/OUT", "float", 2)    		
+	module.createEncoder(QLambda.topicAlpha,"float",1); # alpha config
+	
+	module.createDecoder(QLambda.topicDataOut, "float", noActions)  # decode actions
+	module.createEncoder(QLambda.topicDataIn, "float", noStateVars+1) # encode states (first is reward)
 	return module
-
 

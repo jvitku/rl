9d0c7b72dbacd1b24c4eb3189c1be656a654de29
Jaroslav Vitku <jarda.vitku@gmail.com>
2014-01-11 23:33:45 +0100

    HannsQLambdaVisNengoros done, TODO test it, seems that it breaks after first step

--- sarsa/src/main/java/org/hanns/rl/discrete/ros/sarsa/HannsQLambdaVisNengoros.java
+++ sarsa/src/main/java/org/hanns/rl/discrete/ros/sarsa/HannsQLambdaVisNengoros.java
@@ -1,10 +1,9 @@
 package org.hanns.rl.discrete.ros.sarsa;
 
-import org.hanns.rl.discrete.observer.Observer;
-import org.hanns.rl.discrete.visualizaiton.Visualizer;
-import org.hanns.rl.discrete.visualizaiton.qMatrix.FinalStateSpaceVisDouble;
+import org.hanns.rl.discrete.ros.sarsa.ioHelper.MessageDerivator;
+import org.ros.message.MessageListener;
 import org.ros.node.ConnectedNode;
-import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
 
 import ctu.nengoros.util.SL;
 
@@ -20,80 +19,75 @@ import ctu.nengoros.util.SL;
  * is evaluated as the following case: the action executed did not have effect, RL&ASM: continue.</li>
  * </ul>
  * 
+ * @see {@link org.hanns.rl.discrete.ros.sarsa.ioHelper.MessageDerivationFilter}
+ * 
  * @author Jaroslav Vitku
  *
  */
-public class HannsQLambdaVisNengoros extends HannsQLambda{
-
-	protected FinalStateSpaceVisDouble visualization;
-	protected Publisher<std_msgs.Float32MultiArray> prospPublisher;
-	public static final String topicProsperity = ns+"prosperity";
-
-	SL sl;
+public class HannsQLambdaVisNengoros extends HannsQLambdaVis{
+	
+	protected MessageDerivator filter;
+	
+	public static final String filterConf = "filterLength";
 
 	@Override
-	public void onStart(final ConnectedNode connectedNode) {
-		super.onStart(connectedNode);
-
-		this.registerProsperityPublisher(connectedNode);
-
-		sl = new SL("filex");
-		sl.printToFile(true);
-
-		// initialize the visualizer
-		this.visualization = new FinalStateSpaceVisDouble(
-				states.getDimensionsSizes(), actions.getNumOfActions(), q);
-
-		visualization.setVisPeriod(this.logPeriod);
-		visualization.setTypeVisualization(2);
-		visualization.setActionRemapping(new String[]{"<",">","^","v"});
+	protected void parseParameters(ConnectedNode connectedNode){
+		super.parseParameters(connectedNode);
+		
+		int len = r.getMyInteger(filterConf, MessageDerivator.DEF_MAXLOOP);
+		filter = new MessageDerivator(len);
 	}
 
+	/**
+	 * Well, filter messages directly after receiving them
+	 */
 	@Override
-	protected void performSARSAstep(float reward, float[] state){
-		// store the data into the int[]states
-		super.decodeState(state);	
-		// choose action and learn about it
-		int action = super.learn(reward); 
-		// use observer to log info
-		o.observe(super.prevAction, reward, states.getValues(), action);
+	protected void buildDataIO(ConnectedNode connectedNode){
+		/**
+		 * Action publisher
+		 */
+		actionPublisher =connectedNode.newPublisher(topicDataOut, std_msgs.Float32MultiArray._TYPE);
 
-		//System.out.println("fuuuuu \t\t\talpha"+rl.getConfig().getAlpha()+" "+rl.getConfig().getGamma()+" "+rl.getConfig().getLambda());
+		/**
+		 * State receiver
+		 */
+		Subscriber<std_msgs.Float32MultiArray> dataSub = 
+				connectedNode.newSubscriber(topicDataIn, std_msgs.Float32MultiArray._TYPE);
 
-		// execute action
-		super.executeAction(action);
-		this.publishProsperity();
+		dataSub.addMessageListener(new MessageListener<std_msgs.Float32MultiArray>() {
+			@Override
+			public void onNewMessage(std_msgs.Float32MultiArray message) {
+				float[] data = message.getData();
+				if(data.length != states.getNumVariables()+1)
+					log.error(me+":"+topicDataIn+": Received state description has" +
+							"unexpected length of"+data.length+"! Expected: "+
+							(states.getNumVariables()+1));
+				else{
+					
+					// check whether the message should be passed through
+					boolean shouldPass = filter.newMessageShouldBePassed(data);
+					
+					// here, the state description is decoded and one SARSA step executed
+					if(step % logPeriod==0)
+						myLog(me+"<-"+topicDataIn+" Received new reinforcement &" +
+								" state description "+SL.toStr(data)+" Message should be "
+										+ "processed?: "+shouldPass);
+					
+					if(!shouldPass)
+						return;
 
-		if(this.visualization!=null)
-			this.visualization.performStep(prevAction, reward, states.getValues(), action);
-
-		this.log();
-	}
-
-	public Visualizer getVisualizer(){
-		return this.visualization;
-	}
-
-	private void log(){
-		Observer[] childs = o.getChilds(); 
-		sl.pl(step+" "+o.getProsperity()+" "+childs[0].getProsperity()
-				+" "+childs[1].getProsperity()); // log data to file each step
-
-		if(step%logPeriod ==0)
-			SL.sinfol("step: "+step+" "+o.getProsperity()+"\tcoverage="+childs[0].getProsperity()
-					+" \treward/step="+childs[1].getProsperity()); // log data
-	}
-
-	protected void registerProsperityPublisher(ConnectedNode connectedNode){
-		prospPublisher =connectedNode.newPublisher(topicProsperity, std_msgs.Float32MultiArray._TYPE);
-	}
-
-	protected void publishProsperity(){
-		Observer[] childs = o.getChilds(); 
-		std_msgs.Float32MultiArray fl = prospPublisher.newMessage();	
-		fl.setData(new float[]{o.getProsperity(),childs[0].getProsperity()
-				,childs[1].getProsperity()});								
-		prospPublisher.publish(fl);
+					// decode data (first value is reinforcement..
+					// ..the rest are values of state variables
+					float reward = data[0];
+					float[] state = new float[data.length-1];
+					for(int i=0; i<state.length; i++){
+						state[i] = data[i+1];
+					}
+					// perform the SARSA step
+					performSARSAstep(reward, state);
+				}
+			}
+		});
 	}
 
 }
